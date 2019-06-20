8d56e990fa389723e377e90179a611c7d8f39c59
brittanyhewitson <britt.hewitson@gmail.com>
2019-06-20 06:25:26 -0700

    facial recognition

--- software/process_images.py
+++ software/process_images.py
@@ -8,8 +8,11 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 # Set up logging
-# LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(format=LOGGING_FORMAT, stream=sys.stderr, level=logging.INFO)
+
+def convertToRGB(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 @click.command()
 @click.argument("filename", nargs=1)
@@ -21,7 +24,16 @@ def process_video(filename):
         ret, frame = video.read()
         if frame is None:
             break
-        
+
+        # detect the face
+        logging.info("Detecting facial features")
+        haar_cascade_face = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces_rect = haar_cascade_face.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
+        for (x, y, w, h) in faces_rect:
+            logging.debug(faces_rect)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 15)
+
         cv2.imshow('frame', frame)
         
         # Break if the "q" key is selected
