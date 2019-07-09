8f1ab4ae1a6fc292178dda2168b92b4762b1ffb3
winseyc <winseyc@sfu.ca>
2019-07-09 15:49:51 -0700

    new data

--- software/data_collection/camera.py
+++ software/data_collection/camera.py
@@ -1,11 +1,13 @@
+#pip install "picamera[array]"
 from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
+camera.rotation = 180
 camera.resolution = (640, 480)
 camera.framerate = 32
 camera.start_preview()
-camera.start_recording('wo_blindfold.h264')
-sleep(30)
+camera.start_recording('100mW1m.h264')
+sleep(20)
 camera.stop_recording();
 camera.stop_preview()
