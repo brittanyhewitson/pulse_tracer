dcbb50029f6aedc9dd24003ca321c8bd6123f46a
bhewitso <brittany_hewitson@sfu.ca>
2019-07-18 15:13:21 -0700

    Feat 40

---
+++ software/analysis/heart_rate.py
@@ -0,0 +1,137 @@
+import cv2
+import sys
+import time
+import logging
+import numexpr
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from scipy.signal import butter, filtfilt
+from sklearn.decomposition import FastICA
+
+# Set up logging
+LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(format=LOGGING_FORMAT, stream=sys.stderr, level=logging.INFO)
+
+
+LANDMARK_MAP = {
+    "right_cheek": 31,
+    "left_cheek": 35
+}
+
+class HeartRate(object):
+    def __init__(self, **kwargs):
+        self.frame_rate = 30
+        self.times = []
+        self.data_container = []
+        self.fft = []
+        self.t0 = time.time()
+        self.transformer = FastICA(n_components=1)
+        self.data = kwargs["data"]
+
+    def get_average(self, landmark):
+        try:
+            landmark_id = LANDMARK_MAP[landmark]
+        except KeyError:
+            raise Exception("UNKNOWN LANDMARK ID")
+        
+        red_average = []
+        for roi in self.data:
+            if roi["landmark"] == 35:
+                red_average.append(np.array(roi["red_roi"]).mean())
+        red_average = np.array(red_average)
+        
+        '''
+        plt.plot(red_average)
+        plt.show()
+        plt.clf()
+        '''
+        return red_average
+
+    def normalize(self, mean, std, array):
+        expr = '(array - mean)/std'
+        return numexpr.evaluate(expr)
+    
+    def normalization(self, red_average):
+        mean = red_average.mean()
+        std = np.std(red_average)
+        normalized = self.normalize(mean, std, red_average)
+
+        return normalized
+
+    def get_source(self, red_normal):
+        red_source = self.transformer.fit_transform(red_average.reshape(-1, 1))
+        '''
+        plt.plot(red_source)
+        plt.show()
+        plt.clf()
+        '''
+        return red_source
+
+    def filter(self, source_signal, fs, lowcut, highcut, order):
+        nyq = 0.5 * fs
+        low = lowcut / nyq
+        high = highcut / nyq
+
+        b, a = butter(3, [low, high], btype="bandpass")
+        filtered_signal = filtfilt(b, a, source_signal, axis=0)
+        '''
+        plt.plot(filtered_signal)
+        plt.show()
+        plt.clf()
+        '''
+        return filtered_signal
+
+    def fourier_transform(self, filtered_signal):
+        ft = np.fft.fft(filtered_signal, axis=0)
+        ft = np.fft.fftshift(ft)
+        freq = np.linspace(-30/2, 30/2, num=len(ft))
+        '''
+        plt.plot(freq, abs(ft))
+        plt.show()
+        '''
+        fourier = pd.DataFrame({
+            "freq": freq,
+            "ft": abs(ft)
+        })
+        return fourier
+
+    def get_peak_value(self, fourier_signal):
+        positive_signal = fourier_signal[fourier_signal["freq"] >= 0]
+        max_row = positive_signal.loc[positive_signal["ft"].idxmax()]
+        return max_row["freq"]
+
+
+    def determine_heart_rate(self):
+        # Start with just the red channel for right cheek right now
+        red_average_signal = self.get_average("right_cheek")
+
+        # Normalize the signal
+        normalized_signal = self.normalization(red_average_signal)
+
+        # ICA
+
+        # Bandpass filter the signal
+        filtered_signal = self.filter(
+            source_signal=normalized_signal, 
+            fs=30,
+            lowcut=0.7, 
+            highcut=4,
+            order=5
+        )
+
+        # Get the fourier transform of the signal
+        fourier_signal = self.fourier_transform(filtered_signal)
+
+        # Determine peak value
+        peak_value = self.get_peak_value(fourier_signal)
+
+        # Add thresholding here
+
+        # Get the heart rate in beats per minute
+        heart_rate = peak_value * 60
+        
+        return heart_rate
+
+        
--- software/data_stream/process_images.py
+++ software/data_stream/process_images.py
@@ -1,9 +1,11 @@
 import sys
 import cv2
 import dlib
+import time
 import click
-import timeit
+import json
 import logging
+import numpy as np
 import matplotlib.pyplot as plt
 
 from math import floor, sqrt
@@ -12,186 +14,133 @@ from math import floor, sqrt
 LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(format=LOGGING_FORMAT, stream=sys.stderr, level=logging.INFO)
 
-COLOURS = [
-    (0, 255, 0), 
-    (255, 0, 0),
-    (0, 0, 255),
-    (255, 0, 255)
-]
-
 TRAINED_XMLS = [
-    'data/haarcascade_frontalface_default.xml',
-    'data/haarcascade_frontalface_alt.xml',
-    'data/haarcascade_frontalface_alt2.xml',
-    'data/haarcascade_profileface.xml'
+    '../data/haarcascade_frontalface_default.xml',
+    '../data/haarcascade_frontalface_alt.xml',
+    '../data/haarcascade_frontalface_alt2.xml',
+    '../data/haarcascade_profileface.xml'
 ]
 
-#--------------------------------------------------------------------
-#------------------------- Helper functions -------------------------
-#--------------------------------------------------------------------
-def detect_faces(image, haar_cascade_face):
-    faces_rect = haar_cascade_face.detectMultiScale(
-            image, 
-            scaleFactor=1.1, 
-            minNeighbors=5,
-            minSize=(100, 100)
-        )
-    return faces_rect
+
+class ProcessStream(object):
+    def __init__(self):
+        pass
 
 
-def draw_rectangle(image, faces, colour):
-    for (x, y, w, h) in faces:
-        logging.debug(faces)
-        cv2.rectangle(image, (x, y), (x+w, y+h), colour, 2)
+class ProcessVideo(object):
+    def __init__(self, filename):
+        self.predictor = dlib.shape_predictor("../data/shape_predictor_68_face_landmarks.dat")
+        self.video = cv2.VideoCapture(filename)  
+        self.trained_classifiers = []
+        self.colors = [
+            (0, 255, 0),  # Green
+            (255, 0, 0),  # Blue
+            (0, 0, 255),  # Red
+            (255, 0, 255) # Magenta
+        ]
+        for trained_xml in TRAINED_XMLS:
+            self.trained_classifiers.append(cv2.CascadeClassifier(trained_xml))
+        self.rois = []
+        self.frame_time = 0
+        self.frame = []
+        self.gray_image = []
 
-
-def get_landmark(landmarks, frame, landmark_range, roi_size, vert_translation, hor_translation):
-    rois = []
-    for n in landmark_range:
-        x_n = landmarks.part(n).x
-        y_n = landmarks.part(n).y
-        cv2.circle(frame, (x_n, y_n), 2, (255, 0, 0), -1)
-
-        x1 = x_n + hor_translation + roi_size
-        x2 = x_n + hor_translation - roi_size
-        y1 = y_n + vert_translation + roi_size
-        y2 = y_n + vert_translation - roi_size
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 0), 2)
-        
-        rois.append(frame[x2:x1, y2:y1])
-    return rois
-        
-
-@click.command()
-@click.argument("filename", nargs=1)
-def process_video_cmd(filename):
-    process_video(filename)
-
-
-def process_video(filename):
-    """
-    Image processing steps. Includes facial recognition, 
-    and determining the ROI
-
-    Args:
-        filename:   (str) filepath to the mp4 video to be analyzed
-    """
-    logging.info("Reading video file")
-    video = cv2.VideoCapture(filename)
-
-    # Get the predictor
-    predictor = dlib.shape_predictor("data/shape_predictor_68_face_landmarks.dat")
-    
-    haar_cascade_faces = []
-    for trained_xml in TRAINED_XMLS:
-        haar_cascade_faces.append(cv2.CascadeClassifier(trained_xml))
-
-    # Time the algorithm
-    start = timeit.default_timer()
-    while(True):
-        ret, frame = video.read()
-        if frame is None:
-            break
-        '''
-        # Rotate image (testing only)
+    def rotate_image(self):
         rows, cols, color = frame.shape
         M = cv2.getRotationMatrix2D((cols/2, rows/2), 270, 1)
         frame = cv2.warpAffine(frame, M, (cols, rows))
-        '''
-        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        return frame
 
-        # Try all of the XMLS until a face is detected
-        for i in range(len(TRAINED_XMLS)):
-            faces_rect = detect_faces(gray_image, haar_cascade_faces[i])
-            colour = COLOURS[i]
+    def draw_rectangle(self, faces, color):
+        for (x, y, w, h) in faces:
+            cv2.rectangle(self.frame, (x, y), (x+w, y+h), color, 2)
 
-            if len(faces_rect) > 0:
+    def detect_faces(self):
+        for i in range(len(self.trained_classifiers)):
+            faces = self.trained_classifiers[i].detectMultiScale(
+                self.gray_image, 
+                scaleFactor=1.1,
+                minNeighbors=5, 
+                minSize=(100, 100)
+            )
+            # Break if a face is recognized
+            if len(faces) > 0:
                 break
-        
-        # If no matches found, go to the next frame
-        if len(faces_rect) == 0:
-            continue
 
-        # Compare the new rectangles with the previous frame. Because we are processing
-        # a video at 30fps, there should not be much variation between the two frames. 
-        # Therefore, select the rectangle that is closest to the preious frame
-        #compare_rectangles()
+        # Return nothing if no face could be detected for this frame
+        if len(faces) == 0:
+            return faces
 
-        # Draw the rectangle on the frame (only for testing and demo)
-        draw_rectangle(frame, faces_rect, colour)
+        # Draw the detected face
+        self.draw_rectangle(faces, self.colors[i])
+        return faces
 
-        # Determing facial landmarks
-        for (x, y, w, h) in faces_rect:
+    def get_roi(self, landmarks, landmark_range, roi_side, vert_translation, hor_translation):
+        for n in landmark_range:
+            x_n = landmarks.part(n).x
+            y_n = landmarks.part(n).y
+            cv2.circle(self.frame, (x_n, y_n), 2, (255, 0, 0), -1)
+
+            x1 = x_n + hor_translation + roi_side
+            x2 = x_n + hor_translation - roi_side
+            y1 = y_n + vert_translation + roi_side
+            y2 = y_n + vert_translation - roi_side
+            cv2.rectangle(self.frame, (x1, y1), (x2, y2), (255, 255, 0), 2)
+
+            roi = {
+                'time': self.frame_time,
+                'landmark': n,
+                'red_roi': self.frame[x2:x1, y2:y1, 2].tolist(),
+                'green_roi': self.frame[x2:x1, y2:y1, 1].tolist(),
+                'blue_roi': self.frame[x2:x1, y2:y1, 0].tolist()
+            }
+            self.rois.append(roi)
+
+    def get_landmarks(self, faces):
+        for (x, y, w, h) in faces:
             face = dlib.rectangle(x, y, w+x, h+y)
-            landmarks = predictor(gray_image, face)
-            
-            # Determine how big the regions of interest should be
-            pixel_area = 0.005 * face.area()
-            pixel_size = floor(sqrt(pixel_area)/2)
-            forehead_vert_translation = pixel_size
-            cheek_translation = floor(sqrt(0.05 * face.area())/2)
+            landmarks = self.predictor(self.gray_image, face)
 
-            # Look at landmarks on the bridge of the nose
-            nose_roi = get_landmark(
+            # Determine the size of the ROI based on the detected face
+            roi_area = 0.005 * face.area()
+            roi_side = floor(sqrt(roi_area)/2)
+            forehead_vert_translation = roi_side
+            cheek_hor_translation = floor(sqrt(0.05 * face.area())/2)
+
+            # Get the ROI for the nose
+            self.get_roi(
                 landmarks, 
-                frame, 
                 range(27, 31),
-                pixel_size, 
-                forehead_vert_translation, 
+                roi_side, 
+                forehead_vert_translation,
                 0
             )
-
-            # Look at landmarks on the forehead
-            forehead_roi = get_landmark(
+        
+            # Get the ROI for the forehead
+            self.get_roi(
                 landmarks, 
-                frame, 
                 range(17, 27),
-                pixel_size, 
-                -forehead_vert_translation, 
+                roi_side, 
+                -forehead_vert_translation,
                 0
             )
 
-            # Look at landmarks on the cheeks
-            left_cheek_roi = get_landmark(
+            # Get the ROI for the left cheek
+            self.get_roi(
                 landmarks, 
-                frame, 
-                range(32, 33),
-                pixel_size, 
-                0, 
-                -cheek_translation
+                range(31, 32),
+                roi_side, 
+                0,
+                -cheek_hor_translation
             )
 
-            right_cheek_roi = get_landmark(
+            # Get the ROI for the right cheek
+            self.get_roi(
                 landmarks, 
-                frame, 
                 range(35, 36),
-                pixel_size, 
-                0, 
-                cheek_translation
+                roi_side,
+                0,
+                cheek_hor_translation
             )
-
-            '''
-            for i in right_cheek_roi:
-                cv2.imshow('frame', i)
             break
-            '''
-
-        # Show the detected face
-        cv2.imshow('frame', frame)
-        
-        # Break if the "q" key is selected
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
-    # Log how long the algorithm took
-    stop = timeit.default_timer()
-    total_time = stop - start
-    logging.info("Total time was %s" % total_time)
-    
-    # When everything done, release the capture
-    video.release()
-    cv2.destroyAllWindows()
-
-if __name__=="__main__":
-    process_video_cmd()
-
+            
