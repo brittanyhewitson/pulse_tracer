b4a0d0b47d9abd2f002805e14a779cc89cca78ac
cmyrdal <corey_myrdal@sfu.ca>
2019-07-31 15:59:19 -0700

    Added support to reset primary keys in clean_db.py

--- software/spectrum_metrics/clean_db.py
+++ software/spectrum_metrics/clean_db.py
@@ -7,17 +7,24 @@ password = '@ensc405'
 driver= '{ODBC Driver 17 for SQL Server}'
 
 # Executes SQL on the following tables
-tables = ['pulse_tracer_device','pulse_tracer_healthcare','pulse_tracer_heartrate','pulse_tracer_patient','pulse_tracer_patient_health_care_provider','pulse_tracer_respiratoryrate','pulse_tracer_roi']
+tables = ['pulse_tracer_patient_health_care_provider','pulse_tracer_patient','pulse_tracer_roi','pulse_tracer_device','pulse_tracer_healthcare','pulse_tracer_heartrate','pulse_tracer_respiratoryrate']
 
 # Connect to db
 connection = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = connection.cursor()
 
-# Generates DELETE FROM commands
-queries = [('DELETE FROM dbo.'+table) for table in tables]
+# Generates commands
+queries_delete = [('DELETE FROM dbo.'+table) for table in tables]
+queries_reseed = [('DBCC CHECKIDENT('+table+', RESEED, 0)') for table in tables]
+
 
 # Executes commands on db
-for query in queries:
+for query in queries_delete:
 	cursor.execute(query)
+	
+connection.commit()
 
+for query in queries_reseed:
+	cursor.execute(query)
+	
 connection.commit()
