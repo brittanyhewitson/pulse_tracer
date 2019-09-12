c30426c37e724fc760bf7fd9ec0cb21fdcd6e040
htbui <huy_thong_bui@sfu.ca>
2019-09-11 17:33:15 -0700

    Replace local_pipeline.py

--- software/data_stream/local_pipeline.py
+++ software/data_stream/local_pipeline.py
@@ -4,6 +4,14 @@ import click
 import subprocess
 import logging
 
+import pyodbc
+import config
+import traceback
+import json
+import datetime
+
+
+
 # Set up logging
 LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(format=LOGGING_FORMAT, stream=sys.stderr, level=logging.INFO)
@@ -11,6 +19,17 @@ logging.basicConfig(format=LOGGING_FORMAT, stream=sys.stderr, level=logging.INFO
 @click.command()
 @click.argument("filename", nargs=1)
 @click.argument("roi_locations", nargs=-1)
+
+# do validation and checks before insert
+def validate_string(val):
+   if val != None:
+        if type(val) is int:
+            #for x in val:
+            #   print(x)
+            return str(val).encode('utf-8')
+        else:
+            return val
+
 def main(filename, roi_locations):
     # Check that the file exists
     if not os.path.exists(filename):
@@ -46,6 +65,29 @@ def main(filename, roi_locations):
     # Extract the  Heart and Respiratory Rates
     cmd = f"python ../analysis/run_pipeline.py {json_output}"
     subprocess.check_call(cmd, shell=True)
+	
+	# Database authentication
+	server = 'capstonesfu.database.windows.net'
+	database = 'spectrum_metrics'
+	username = 'team2'
+	password = '@ensc405'
+	driver= '{ODBC Driver 17 for SQL Server}'
+	cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+	cursor = cnxn.cursor()
+	
+	# Row iteration through json_file and insert into database
+	for i, item in enumerate(json_obj):
+		time = item.get("time", None)
+		landmark = item.get("landmark", None)
+		blue_data = item.get("blue_roi", None)
+		blue_data_str = str(blue_data)
+	   	green_data = validate_string(item.get("green_roi", None))
+		green_data_str = str(green_data)
+		red_data = validate_string(item.get("red_roi", None))
+		red_data_str = str(red_data)
+	 	cursor.execute("INSERT INTO dbo.testing([time],landmark,blue_data,green_data,red_data) VALUES (?,?,?,?,?)", (time,landmark,blue_data_str,green_data_str,red_data_str))
+
+cnxn.commit()
 
 if __name__=='__main__':
     main()
