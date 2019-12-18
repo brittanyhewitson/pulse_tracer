29ec29a390d00bca149c1a08061f7281c1061260
brittanyhewitson <britt.hewitson@gmail.com>
2019-12-18 13:21:12 -0800

    reorganized repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='pulse_tracer',
+	description='Patient monitoring system',
+	author='Brittany Hewitson',
+    packages=find_packages(),
+	zip_safe=False,
+)
--- software/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-	name='pulse_tracer',
-	description='Patient monitoring system',
-	author='Brittany Hewitson',
-    packages=find_packages(),
-	zip_safe=False,
-)
